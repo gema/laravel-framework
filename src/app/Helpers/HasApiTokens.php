a7e250e55a7365a071c5bb4e755592d958dc40b0
Antonio Almeida <promatik@gmail.com>
2020-01-06 12:21:45 +0000

    Added HasApiTokens Helper Package update

---
+++ src/app/Helpers/HasApiTokens.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace GemaDigital\Framework\App\Helpers;
+
+use Config;
+use Laravel\Passport\HasApiTokens as OriginalHasApiTokens;
+
+trait HasApiTokens
+{
+    use OriginalHasApiTokens {
+        OriginalHasApiTokens::token as parentToken;
+    }
+
+    /**
+     * Get the current access token being used by the user.
+     *
+     * @return \Laravel\Passport\Token|null
+     */
+    function token()
+    {
+        return $this->parentToken() ?: $this->createToken(Config::get('app.name'))->accessToken;
+    }
+}
