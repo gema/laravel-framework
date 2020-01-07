2ea8ac16352e2c49d91ee86a31ebb57d41e6b9be
Filipa Simão <filipa.simao@heydigital.pt>
2020-01-07 15:13:59 +0000

    Remove filters from user crud controller

--- src/app/Http/Controllers/Admin/UserCrudController.php
+++ src/app/Http/Controllers/Admin/UserCrudController.php
@@ -16,37 +16,6 @@ class UserCrudController extends \Backpack\PermissionManager\app\Http\Controller
     public function setupListOperation()
     {
         parent::setupListOperation();
-
-        // Filters
-        $this->crud->addFilter([
-            'name' => 'roles',
-            'type' => 'select2_multiple',
-            'label' => ucfirst(__('backpack::permissionmanager.roles')),
-            'placeholder' => __('Select a role'),
-        ],
-            EnumHelper::translate('user.roles'),
-            function ($values) {
-                $this->crud->query->whereHas('roles', function ($query) use ($values) {
-                    $query
-                        ->selectRaw('role_id')
-                        ->whereIn('role_id', json_decode($values));
-                });
-            });
-
-        $this->crud->addFilter([
-            'name' => 'permissions',
-            'type' => 'select2_multiple',
-            'label' => ucfirst(__('backpack::permissionmanager.permission_plural')),
-            'placeholder' => __('Select a permission'),
-        ],
-            EnumHelper::translate('user.permissions'),
-            function ($values) {
-                $this->crud->query->whereHas('permissions', function ($query) use ($values) {
-                    $query
-                        ->selectRaw('permission_id')
-                        ->whereIn('permission_id', json_decode($values));
-                });
-            });
     }
 
     public function setupFields()
