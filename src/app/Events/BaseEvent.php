78a72f68e27fa0b6777567e7f0ae54e353218675
Antonio Almeida <promatik@gmail.com>
2020-04-23 14:44:55 +0100

    Added channel, user and timestamp to BaseEvent

--- src/app/Events/BaseEvent.php
+++ src/app/Events/BaseEvent.php
@@ -3,6 +3,7 @@
 namespace GemaDigital\Framework\App\Events;
 
 use Auth;
+use Carbon\Carbon;
 use GemaDigital\Framework\App\Events\SerializesEvents;
 use GemaDigital\Framework\App\Events\ShouldBroadcast;
 use Illuminate\Broadcasting\InteractsWithSockets;
@@ -13,13 +14,34 @@ abstract class BaseEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels, SerializesEvents;
 
+    public $channel;
+    public $user;
+    public $timestamp;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(string $channel)
+    {
+        $this->channel = $channel;
+        $this->user = $this->getUserData();
+        $this->timestamp = Carbon::now();
+    }
+
     public function broadcastAs()
     {
         return (new \ReflectionClass($this))->getShortName();
     }
 
+    public function getChannel()
+    {
+        return $this->channel;
+    }
+
     public function getUserData()
     {
-        return (object) Auth::user()->only(['id', 'name', 'email']);
+        return (object) Auth::user()->only(['id', 'name', 'email']) ?? null;
     }
 }
