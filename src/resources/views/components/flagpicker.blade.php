a4c0092860a40ac6a9acadb18babc66697c03a5a
Antonio Almeida <promatik@gmail.com>
2020-05-01 18:33:16 +0100

    Added translate as an arg for flagpicker Removed odd space on flagpicker

--- src/resources/views/components/flagpicker.blade.php
+++ src/resources/views/components/flagpicker.blade.php
@@ -1,10 +1,14 @@
 @php
 $lang = Session::get('locale', 'en');
 $locales = config('backpack.crud.locales');
+$translate = $translate ?? null;
 @endphp
 
 @foreach($locales as $local => $label)
-<a href="{{ route('lang', ['locale' => $local]) }}" class="lang{{ $local == $lang ? " active" : "" }}">
-    <span>{{ strtoupper(__("$local")) }}</span>
-</a>
+<a href="{{ route('lang', ['locale' => $local]) }}" class="lang{{ $local == $lang ? " active" : "" }}" style="text-transform: uppercase;">
+    @if(!$translate)
+    {{ $local }}</a>
+    @else
+    {{ Locale::getDisplayLanguage($local, $local) }}</a>
+    @endif
 @endforeach
