ae49de45e98a658ca5236529eea342c29376655b
Antonio Almeida <promatik@gmail.com>
2020-05-03 22:56:53 +0100

    Added base Model and SaveMedia trait to framework

---
+++ src/app/Models/Model.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace GemaDigital\Framework\App\Models;
+
+class Model extends \Illuminate\Database\Eloquent\Model
+{
+    // Scopes
+    public static function find(string $id)
+    {
+        return static::where('id', $id)->firstOrFail();
+    }
+
+    public function scopePublished($query)
+    {
+        return $query->where('status', 'publish');
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('active', true);
+    }
+
+    public function scopeOrdered($query)
+    {
+        return $query->orderBy('lft', 'ASC');
+    }
+
+    // Link Helper
+    public function getLink($entity)
+    {
+        if (!$entity) {
+            return '-';
+        }
+
+        $class = strtolower(class_basename($entity));
+        return "<a href='/admin/$class/{$entity->id}/edit'>" . str_limit($entity->name, 60, '...') . '</a>';
+    }
+
+    // Upload Multiple Files Helper
+    public function uploadMultipleFilesToDiskKeepFileName($value, $attribute_name, $disk, $destination_path)
+    {
+        $request = \Request::instance();
+        $attribute_value = (array) $this->{$attribute_name};
+        $files_to_clear = $request->get('clear_' . $attribute_name);
+
+        // if a file has been marked for removal,
+        // delete it from the disk and from the db
+        if ($files_to_clear) {
+            $attribute_value = (array) $this->{$attribute_name};
+            foreach ($files_to_clear as $key => $filename) {
+                \Storage::disk($disk)->delete($filename);
+                $attribute_value = array_where($attribute_value, function ($value, $key) use ($filename) {
+                    return $value != $filename;
+                });
+            }
+        }
+
+        // if a new file is uploaded, store it on disk and its filename in the database
+        if ($request->hasFile($attribute_name)) {
+            foreach ($request->file($attribute_name) as $file) {
+                if ($file->isValid()) {
+                    // 1. Generate a new file name
+                    $new_file_name = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME) . '.' . time() . '.' . $file->getClientOriginalExtension();
+
+                    // 2. Move the new file to the correct path
+                    $file_path = $file->storeAs($destination_path, $new_file_name, $disk);
+
+                    // 3. Add the public path to the database
+                    $attribute_value[] = $file_path;
+                }
+            }
+        }
+
+        $this->attributes[$attribute_name] = json_encode($attribute_value);
+    }
+}
---
+++ src/app/Models/Traits/SaveMedia.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace GemaDigital\Framework\App\Models\Traits;
+
+use Carbon\Carbon;
+use Config;
+use Image;
+use Storage;
+
+trait SaveMedia
+{
+    public function saveImage($model, $value, $path, $name, $sizes, $quality = 85, $attribute_name = 'image', $disk = 'uploads')
+    {
+        // if the image was erased
+        if ($value == null) {
+            Storage::disk($disk)->delete($model->{$attribute_name});
+            $model->attributes[$attribute_name] = null;
+        }
+
+        // if a base64 was sent, store it in the db
+        if (starts_with($value, 'data:image')) {
+            $format = substr($value, 11, 3);
+
+            // jpeg exception
+            if ($format == 'jpe') {
+                $format = 'jpg';
+            }
+
+            $timestamp = Carbon::now()->timestamp;
+            $filename = str_slug("$name $timestamp") . ".$format";
+
+            $image = Image::make($value);
+
+            // Save original image
+            Storage::disk($disk)->put($path . $filename, $image->stream($format, $quality));
+            $model->attributes[$attribute_name] = str_replace(Config::get('app.url'), '', $path . $filename);
+
+            // Save all sizes
+            if (!is_array($sizes)) {
+                $sizes = [$sizes];
+            }
+
+            rsort($sizes);
+
+            foreach ($sizes as $size) {
+                if ($image->width() > $size) {
+                    $image->resize($size, null, function ($c) {$c->aspectRatio();});
+                }
+
+                Storage::disk($disk)->put("$path/$size/$filename", $image->stream($format, $quality));
+            }
+
+            return $model->attributes[$attribute_name];
+        } else {
+            $model->attributes[$attribute_name] = $value;
+        }
+
+        // Clean path
+        return $model->attributes[$attribute_name] = $this->cleanPath($model->attributes[$attribute_name], $disk);
+    }
+
+    public function cleanPath($path, $disk)
+    {
+        // Remove URL
+        $path = str_replace(Config::get('app.url'), '', $path);
+
+        // Remove Disk
+        if ($disk) {
+            $path = preg_replace("/\/?$disk\/?/", '', $path);
+        }
+
+        // Remove First slash
+        $path = preg_replace("/^\//", '', $path);
+
+        return $path;
+    }
+}
