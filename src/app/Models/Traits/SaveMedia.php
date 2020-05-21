18bd1cbb4b886b500af3f760f255080595b46466
Antonio Almeida <promatik@gmail.com>
2020-05-21 18:03:24 +0100

    Moved uploadMultipleFilesToDiskKeepFileName to SaveMedia Trait

--- src/app/Models/Traits/SaveMedia.php
+++ src/app/Models/Traits/SaveMedia.php
@@ -75,4 +75,41 @@ trait SaveMedia
 
         return $path;
     }
+
+    public function uploadMultipleFilesToDiskKeepFileName($value, $attribute_name, $disk, $destination_path)
+    {
+        $request = \Request::instance();
+        $attribute_value = (array) $this->{$attribute_name};
+        $files_to_clear = $request->get('clear_' . $attribute_name);
+
+        // if a file has been marked for removal,
+        // delete it from the disk and from the db
+        if ($files_to_clear) {
+            $attribute_value = (array) $this->{$attribute_name};
+            foreach ($files_to_clear as $key => $filename) {
+                \Storage::disk($disk)->delete($filename);
+                $attribute_value = array_where($attribute_value, function ($value, $key) use ($filename) {
+                    return $value != $filename;
+                });
+            }
+        }
+
+        // if a new file is uploaded, store it on disk and its filename in the database
+        if ($request->hasFile($attribute_name)) {
+            foreach ($request->file($attribute_name) as $file) {
+                if ($file->isValid()) {
+                    // 1. Generate a new file name
+                    $new_file_name = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME) . '.' . time() . '.' . $file->getClientOriginalExtension();
+
+                    // 2. Move the new file to the correct path
+                    $file_path = $file->storeAs($destination_path, $new_file_name, $disk);
+
+                    // 3. Add the public path to the database
+                    $attribute_value[] = $file_path;
+                }
+            }
+        }
+
+        $this->attributes[$attribute_name] = json_encode($attribute_value);
+    }
 }
