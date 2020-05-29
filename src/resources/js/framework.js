36e7c0b3fc033350337037c002f802c79acf8c12
Antonio Almeida <promatik@gmail.com>
2020-05-29 14:43:06 +0100

    Added framework compatibility with ESLint

--- src/resources/js/framework.js
+++ src/resources/js/framework.js
@@ -1,39 +1,40 @@
 // Prototype
-NodeList.prototype.__proto__ = HTMLCollection.prototype.__proto__ = Array.prototype;
+Object.setPrototypeOf(NodeList.prototype, Array.prototype);
+Object.setPrototypeOf(HTMLCollection.prototype, Array.prototype);
 
 // Query
 window.query = document.querySelector.bind(document);
 window.queryAll = document.querySelectorAll.bind(document);
-Node.prototype.query = function(selector) { return this.querySelector(selector) }
-Node.prototype.queryAll = function(selector) { return this.querySelectorAll(selector) }
-NodeList.prototype.query = function(selector) { return this.queryAll(selector)[0] }
-NodeList.prototype.queryAll = function(selector) {
-    let results = [];
-    this.map(elem => results.push(...elem.first(selector)));
-    return results;
-}
+Node.prototype.query = function query(selector) { return this.querySelector(selector); };
+Node.prototype.queryAll = function queryAll(selector) { return this.querySelectorAll(selector); };
+NodeList.prototype.query = function query(selector) { return this.queryAll(selector)[0]; };
+NodeList.prototype.queryAll = function queryAll(selector) {
+  const results = [];
+  this.map((elem) => results.push(...elem.first(selector)));
+  return results;
+};
 
 // Utils
-Node.prototype.sibling = function(query) { return this.siblings(query)[0] }
-Node.prototype.siblings = function(query) {
-	let elems = query ? this.parentElement.queryAll(query) : this.parentElement.children;
-	return elems.filter(e => this != e);
-}
+Node.prototype.sibling = function sibling(query) { return this.siblings(query)[0]; };
+Node.prototype.siblings = function siblings(query) {
+  const elems = query ? this.parentElement.queryAll(query) : this.parentElement.children;
+  return elems.filter((e) => this !== e);
+};
 
 // Index of node
-Node.prototype.index = function() {
-    let elem = this, i = 0;
-    while(elem = elem.previousElementSibling)
-        i++;
-    return i;
-}
+Node.prototype.index = function index() {
+  let elem = this;
+  let i = 0;
+  while (elem) { elem = elem.previousElementSibling; i++; }
+  return i;
+};
 
 // App
 export function template(selector) {
-    return query(selector).content.cloneNode(true);
+  return query(selector).content.cloneNode(true);
 }
 
 // Admin panel
 export function adminShortcut() {
-    window.addEventListener("keypress", e => e.shiftKey && e.keyCode == 88 && (window.location.href = '/admin'), false);
+  window.addEventListener('keypress', (e) => e.shiftKey && e.keyCode === 88 && (window.location.href = '/admin'), false);
 }
