b0f813c466f5cee600ec5ab8a5fd0e069003953e
Antonio Almeida <promatik@gmail.com>
2020-11-22 18:43:59 +0000

    Improved intersection observer

--- src/resources/js/libs/intersection-observer.js
+++ src/resources/js/libs/intersection-observer.js
@@ -1,14 +1,22 @@
 // Interception observer
 // eslint-disable-next-line import/prefer-default-export
 export function init() {
-  queryAll('.observable').forEach(observable => {
-    const threshold = parseFloat(observable.getAttribute('threshold'));
+  queryAll('.observable, [observable]').forEach(observable => {
+    let status = false;
+    const threshold = parseFloat(observable.getAttribute('threshold')) || 1;
+    const once = observable.getAttribute('once') !== null;
 
     const revealContentObserver = new IntersectionObserver(entries => {
       entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.dispatchEvent(new CustomEvent('observed'));
-          revealContentObserver.unobserve(entry.target);
+        if (entry.isIntersecting && !status) {
+          status = true;
+          entry.target.dispatchEvent(new CustomEvent('observed', entry));
+          if (once) {
+            revealContentObserver.unobserve(entry.target);
+          }
+        } else if (!entry.isIntersecting && status) {
+          status = false;
+          entry.target.dispatchEvent(new CustomEvent('unobserved', entry));
         }
       });
     }, { threshold });
