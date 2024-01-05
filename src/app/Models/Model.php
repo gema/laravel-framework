f55a0ea91430c4a97f3be3af7b475d86d9aed78b
Antonio Almeida <promatik@gmail.com>
2024-01-05 00:05:02 +0000

    Trigger sync method on save too

--- src/app/Models/Model.php
+++ src/app/Models/Model.php
@@ -14,18 +14,18 @@ class Model extends OriginalModel
         /*
          * Retrieve
          */
-        method_exists(__CLASS__, 'retrieved') && static::retrieved(function ($element) {
+        method_exists(__CLASS__, 'retrieved') && static::retrieved(function ($element): void {
             self::call($element, 'afterRetrieve');
         });
 
         /*
          * Create
          */
-        method_exists(__CLASS__, 'creating') && static::creating(function ($element) {
+        method_exists(__CLASS__, 'creating') && static::creating(function ($element): void {
             self::call($element, 'beforeCreate');
         });
 
-        method_exists(__CLASS__, 'created') && static::created(function ($element) {
+        method_exists(__CLASS__, 'created') && static::created(function ($element): void {
             self::call($element, 'afterCreate');
             self::call($element, 'sync', 'create');
         });
@@ -33,11 +33,11 @@ class Model extends OriginalModel
         /*
          * Update
          */
-        method_exists(__CLASS__, 'updating') && static::updating(function ($element) {
+        method_exists(__CLASS__, 'updating') && static::updating(function ($element): void {
             self::call($element, 'beforeUpdate');
         });
 
-        method_exists(__CLASS__, 'updated') && static::updated(function ($element) {
+        method_exists(__CLASS__, 'updated') && static::updated(function ($element): void {
             self::call($element, 'afterUpdate');
             self::call($element, 'sync', 'update');
         });
@@ -45,35 +45,39 @@ class Model extends OriginalModel
         /*
          * Save
          */
-        method_exists(__CLASS__, 'saving') && static::saving(function ($element) {
+        method_exists(__CLASS__, 'saving') && static::saving(function ($element): void {
             self::call($element, 'beforeSave');
         });
 
-        method_exists(__CLASS__, 'saved') && static::saved(function ($element) {
+        method_exists(__CLASS__, 'saved') && static::saved(function ($element): void {
             self::call($element, 'afterSave');
+            self::call($element, 'sync', 'saved');
         });
 
         /*
          * Delete
          */
-        method_exists(__CLASS__, 'deleting') && static::deleting(function ($element) {
+        method_exists(__CLASS__, 'deleting') && static::deleting(function ($element): void {
             self::call($element, 'beforeDelete');
             self::call($element, 'sync', 'delete');
         });
 
-        method_exists(__CLASS__, 'deleted') && static::deleted(function ($element) {
+        method_exists(__CLASS__, 'deleted') && static::deleted(function ($element): void {
             self::call($element, 'afterDelete');
         });
 
         /*
          * Retrieve
          */
-        method_exists(__CLASS__, 'retrieved') && static::retrieved(function ($element) {
+        method_exists(__CLASS__, 'retrieved') && static::retrieved(function ($element): void {
             self::call($element, 'beforeRetrieved');
         });
     }
 
-    protected static function call($element, $method, $extra = null)
+    /**
+     * Call method if exists.
+     */
+    protected static function call($element, string $method, string $extra = null): void
     {
         if (method_exists($element, $method)) {
             $element->{$method}($element, $extra);
