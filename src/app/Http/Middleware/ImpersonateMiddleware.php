3c4ac6ae9e52d057bb5d092311a44fc670e4687b
António Almeida <promatik@gmail.com>
2025-04-27 22:16:17 +0100

    feat: Add ImpersonateMiddleware to handle user impersonation

---
+++ src/app/Http/Middleware/ImpersonateMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace GemaDigital\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class ImpersonateMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Session::has('impersonated')) {
+            Auth::onceUsingId(Session::get('impersonated'));
+        }
+
+        return $next($request);
+    }
+}
