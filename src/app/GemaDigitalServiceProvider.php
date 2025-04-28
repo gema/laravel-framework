8a6e6ac284760a4561688231fd1b5596ef6a4503
António Almeida <promatik@gmail.com>
2025-04-28 01:16:41 +0100

    fix: Update loadHelpers method to require GlobalHelper.php instead of CommonHelper.php

--- src/app/GemaDigitalServiceProvider.php
+++ src/app/GemaDigitalServiceProvider.php
@@ -90,7 +90,7 @@ class GemaDigitalServiceProvider extends ServiceProvider
      */
     public function loadHelpers(): void
     {
-        require_once __DIR__.'/Helpers/CommonHelper.php';
+        require_once __DIR__.'/Helpers/GlobalHelper.php';
     }
 
     /**
