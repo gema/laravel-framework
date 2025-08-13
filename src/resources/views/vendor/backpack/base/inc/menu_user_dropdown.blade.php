73c53533ae057aebc13d4731130a12f409f13948
António Almeida <promatik@gmail.com>
2025-08-14 02:01:58 +0900

    fix: Remove terminal link from user dropdown menu

--- src/resources/views/vendor/backpack/base/inc/menu_user_dropdown.blade.php
+++ src/resources/views/vendor/backpack/base/inc/menu_user_dropdown.blade.php
@@ -3,6 +3,5 @@
 <a class="dropdown-item" href="{{ route('impersonate.leave') }}"><i class="la la-user-secret me-2"></i> {{ ucfirst(__("gemadigital::messages.leave_impersonation")) }}</a>
 @endif
 <a class="dropdown-item" href="{{ route('actions') }}"><i class="la la-key me-2"></i> {{ __("gemadigital::messages.admin_actions") }}</a>
-<a class="dropdown-item" href="{{ route('terminal') }}"><i class="la la-terminal me-2"></i> {{ __("gemadigital::messages.artisan_terminal") }}</a>
 <div class="dropdown-divider"></div>
 @endif
