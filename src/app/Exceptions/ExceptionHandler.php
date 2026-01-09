ed5f7c596e4950556b7936cfec222890c7bdb277
António Almeida <promatik@gmail.com>
2026-01-09 19:41:56 +0800

    Added PgSQL not found validation

--- src/app/Exceptions/ExceptionHandler.php
+++ src/app/Exceptions/ExceptionHandler.php
@@ -7,6 +7,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class ExceptionHandler
@@ -21,6 +22,11 @@ class ExceptionHandler
      */
     public static function render(Throwable $exception, Request $request): Response|false
     {
+        if ($exception->getCode() === '22P02') {
+            $message =  str_replace("ERROR:  ", '', explode("\n", $exception->errorInfo[2])[0]);
+            throw new NotFoundHttpException($message);
+        }
+
         if (app()->hasDebugModeEnabled() && $request->expectsJson()) {
             $name = basename($exception::class);
             $file = preg_replace('/\\\/', '/', str_replace(base_path(), '', $exception->getFile()));
