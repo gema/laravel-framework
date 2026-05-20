a7eb58717897527bee4de1555484446081cf9fb1
António Almeida <promatik@gmail.com>
2026-05-21 01:15:50 +0800

    feat: Implement XDeleteOperation trait for enhanced delete functionality

---
+++ src/app/Http/Controllers/Admin/Operations/XDeleteOperation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace GemaDigital\Http\Controllers\Admin\Operations;
+
+use GemaDigital\Http\Controllers\Admin\CrudController;
+
+/** @phpstan-ignore trait.unused */
+trait XDeleteOperation
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation {
+        destroy as destroyTrait;
+    }
+
+    public function destroy($id)
+    {
+        $result = $this->destroyTrait($id);
+        $this->sync(CrudController::DESTROYED);
+
+        return $result;
+    }
+}
